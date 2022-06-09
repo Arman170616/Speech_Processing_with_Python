92966abec02961b0467d29e1f0dca097e587d709
Arman170616 <armanice09@gmail.com>
2022-06-09 10:40:03 +0600

    audio signal parameters

---
+++ 01-basic/01wave.py
@@ -0,0 +1,15 @@
+# audio file format
+# .mp3  
+# .flac    
+# .wav  
+import wave
+
+'''
+Audio Signal Parameters
+ -number of chennels
+ -sample width
+ -framerate/sample_rate
+ -number of frames
+ -values of a frame
+'''
+
